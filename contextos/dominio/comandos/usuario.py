a7f7566f1d3d58c5e09dc11dbe76821f3878b0d8
lucianogomes02 <luciano@pontotel.com.br>
2022-08-19 02:21:11 -0300

    feat(contextos/dominio/comandos/usuario.py): definição do id_usuario nos Comandos

--- contextos/dominio/comandos/usuario.py
+++ contextos/dominio/comandos/usuario.py
@@ -1,27 +1,28 @@
 from dataclasses import dataclass
 from . import Comando
+from libs.usuario import IdUsuario
 
 
 @dataclass
 class CadastrarUsuario(Comando):
-    pass
+    id_usuario: IdUsuario
 
 
 @dataclass
 class AlterarUsuario(Comando):
-    pass
+    id_usuario: IdUsuario
 
 
 @dataclass
 class RemoverUsuario(Comando):
-    pass
+    id_usuario: IdUsuario
 
 
 @dataclass
 class LogarUsuario(Comando):
-    pass
+    id_usuario: IdUsuario
 
 
 @dataclass
 class DeslogarUsuario(Comando):
-    pass
+    id_usuario: IdUsuario
