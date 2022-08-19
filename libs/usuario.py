66ea0d23f1a44223bab46aaab0e6c5e70e2c5242
lucianogomes02 <luciano@pontotel.com.br>
2022-08-19 05:04:40 -0300

    feat(libs/usuario.py): implementa validações para o tipo Numero

--- libs/usuario.py
+++ libs/usuario.py
@@ -27,17 +27,40 @@ class Logradouro(str):
     def _verificar_tipo(cls, logradouro: str):
         if not isinstance(logradouro, str):
             raise cls.TipoInvalido(
-                "O logradouro precisa ser criado a partir de um tipo float."
+                "O logradouro precisa ser criado a partir de um tipo string/texto."
             )
 
     @classmethod
     def _verificar_logradouro(cls, logradouro: str):
-        if not logradouro and not logradouro.isalnum():
-            raise cls.LogradouroInvalido("Logradouro inválido! Verifique o logradouro e tente novamente.")
+        if not logradouro:
+            raise cls.LogradouroInvalido("Logradouro não pode estar vázio.")
 
 
 class Numero(str):
-    pass
+    class TipoInvalido(Exception):
+        pass
+
+    class NumeroInvalido(Exception):
+        pass
+
+    def __new__(cls, numero: str):
+        return super().__new__(cls, numero)
+
+    def __init__(self, numero: str):
+        self._verificar_tipo(numero)
+        self._verificar_numero(numero)
+
+    @classmethod
+    def _verificar_tipo(cls, numero: str):
+        if not isinstance(numero, str):
+            raise cls.TipoInvalido(
+                "O número precisa ser criado a partir de um tipo string/texto."
+            )
+
+    @classmethod
+    def _verificar_numero(cls, numero: str):
+        if not numero and not numero.isdigit():
+            raise cls.NumeroInvalido("Número inválido! Apenas carácteres númericos são permitidos.")
 
 
 class Complemento(str):
